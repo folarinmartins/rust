76cd58e06d990c1de2417c44dcb7afb14e57a4cb
Martins Folarin <folarin@engineer.com>
2025-10-11 19:11:43 -0400

    feat(intro): created a cli-based number guess game

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,28 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    println!("I have a number, please share your guess:");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    
+    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
+    println!("I have a number guess what it is?");
     loop {
         let mut input = String::new();
         io::stdin()
             .read_line(&mut input)
-            .expect("Failed to read input");
-            
+            .expect("Unable to parse input");
+
         let guess: i32 = match input.trim().parse() {
-              Ok(num) => num,
-              Err(_) => continue,
-            };
-          
-        match guess.cmp(&secret_number){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}??? Too Small", guess),
             Ordering::Equal => {
-              println!("Spot on!");
-              break;
+                println!("Spot on!");
+                break;
             }
-            Ordering::Less => println!("Come on, {} is too low!", guess),
-            Ordering::Greater => println!("{}...too high!", guess),
+            Ordering::Greater => println!("Too much, champ!"),
         }
     }
 }
